db75b03ff578080aafc1ad29ff2b832e1e8c2e37
starmetric <na0z.lineage2@gmail.com>
2026-03-28 01:13:00 +0700

    Use a struct to solve the problem of storing data in batches to create loops and add products without forgetting previous data.

--- pos.c
+++ pos.c
@@ -1,44 +1,49 @@
 #include<stdio.h>
 #include<string.h>
 
+
+    struct Item {
+      char product[10];
+      int unit;
+      int total;
+    };
+
 int main(){
-    
+
+    struct Item item;
     int select;
-    int unit[4];
-    int total;
-    char product[10];
     int running = 1;
+
 
     printf("\tHello, welcome\n\n\tStarmetricshop\n\n[1]Productlist [2]Contactmembership [3]Exit\n");
     printf("select Number : ");
     scanf("%d", &select);
 
     while (running == 1) {
-    
     if (select == 1) {
-        printf("\n\n\tProduct\negg / 6THB/1unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
+        printf("\n\n\tProduct\negg / 6THB/1item.unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
           printf("Select Product = ");
-          scanf("%7s", product);
-          if (strcmp(product, "egg")== 0) {
-              printf("Required Quantity (unit) = ");
-              scanf("%d", &unit[0]);
-              total = unit[0] * 6;
-              printf("Total %s = %d THB\n", product, total);
-          }else if (strcmp(product, "pork")== 0) {
+          scanf("%7s", item.product);
+          if (strcmp(item.product, "egg")== 0) {
+              printf("Required Quantity (item.unit) = ");
+              scanf("%d", &item.unit);
+              item.total = item.unit * 6;
+              printf("Total %s = %d THB\n", item.product, item.total);
+          }else if (strcmp(item.product, "pork")== 0) {
               printf("Required Quantity (g) = ");
-              scanf("%d", &unit[1]);
-              total = (unit[1] * 46) / 100;
-              printf("Total %s = %d THB\n", product, total);
-          }else if (strcmp(product, "fish")== 0) {
+              scanf("%d", &item.unit);
+              item.total = (item.unit * 46) / 100;
+              printf("Total %s = %d THB\n", item.product, item.total);
+          }else if (strcmp(item.product, "fish")== 0) {
               printf("Required Quantity (g) = ");
-              scanf("%d", &unit[2]);
-              total = (unit[2] * 28) / 100;
-              printf("Total %s = %d THB\n", product, total);
-          }else if (strcmp(product, "duck")== 0) {
+              scanf("%d", &item.unit);
+              item.total = (item.unit * 28) / 100;
+              printf("Total %s = %d THB\n", item.product, item.total);
+          }else if (strcmp(item.product, "duck")== 0) {
               printf("Required Quantity (g) = ");
-              scanf("%d", &unit[3]);
-              total = (unit[3] * 68) / 100;
-              printf("Total %s = %d THB\n", product, total);
+              scanf("%d", &item.unit);
+              item.total = (item.unit * 68) / 100;
+              printf("Total %s = %d THB\n", item.product, item.total);
           }    
     }else if (select == 2) {
         printf("\n\n[1]Contact [2]Membership\n");
