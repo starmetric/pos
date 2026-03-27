182fc0ea1bb265a39245b55e4db7074c7874389f
starmetric <na0z.lineage2@gmail.com>
2026-03-27 22:17:53 +0700

    add select 1 char quantity, printf, scanf (Required quantity,quantity)

--- pos.c
+++ pos.c
@@ -4,15 +4,18 @@ int main(){
     
     int select;
     char product[10];
+    char quantity[6];
 
     printf("\tHello, welcome\n\n\tStarmetricshop\n\n[1]Productlist [2]Contactmembership [3]Exit\n");
-    printf("selectnumber : ");
+    printf("select Number : ");
     scanf("%d", &select);
 
     if (select == 1) {
         printf("\n\n\tProduct\negg / 6THB/1unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
-          printf("selectProduct = ");
+          printf("Select Product = ");
           scanf("%s", product);
+          printf("Required Quantity= ");
+          scanf("%s", quantity);
     }else if (select == 2) {
         printf("\n\n[1]Contact [2]Membership\n");
     }else 
