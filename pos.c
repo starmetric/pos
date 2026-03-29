c5acac62aa8739a96d36d04bb3383f3ce5fc919b
starmetric <na0z.lineage2@gmail.com>
2026-03-29 18:02:59 +0700

    Move the while loop to below the int main declaration.

--- pos.c
+++ pos.c
@@ -13,17 +13,18 @@ int main(){
     int select;
     int running = 1;
 
+    while (running == 1){
+
     printf("\tHello, welcome\n\n\tStarmetricshop\n\n[1]Productlist [2]Contactmembership [3]Exit\n");
     printf("select Number : ");
     scanf("%d", &select);
 
-    while (running == 1) {
     if (select == 1) {
-        printf("\n\n\tProduct\negg / 6THB/1v1.unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
+        printf("\n\n\tProduct\negg / 6THB/unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
           printf("Select Product = ");
           scanf("%7s", v1.product);
           if (strcmp(v1.product, "egg")== 0) {
-              printf("Required Quantity (v1.unit) = ");
+              printf("Required Quantity (unit) = ");
               scanf("%d", &v1.unit);
               v1.total = v1.unit * 6;
               printf("Total %s = %d THB\n", v1.product, v1.total);
