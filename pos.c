926b1b92713ea5b17baece5a34faacae9ecb2c6a
starmetric <na0z.lineage2@gmail.com>
2026-03-29 17:43:58 +0700

    I created a struct named structvisit, using Typedef for easy calling, and then declared an instance named v1 to store product information.

--- pos.c
+++ pos.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 
+typedef struct {
+    char product[10];
+    int unit;
+    int total;
+} structvisit;
 
-    struct Item {
-      char product[10];
-      int unit;
-      int total;
-    };
-
+    
 int main(){
-
-    struct Item item;
+    structvisit v1;
     int select;
     int running = 1;
-
 
     printf("\tHello, welcome\n\n\tStarmetricshop\n\n[1]Productlist [2]Contactmembership [3]Exit\n");
     printf("select Number : ");
@@ -21,29 +19,29 @@ int main(){
 
     while (running == 1) {
     if (select == 1) {
-        printf("\n\n\tProduct\negg / 6THB/1item.unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
+        printf("\n\n\tProduct\negg / 6THB/1v1.unit\npork / 46THB/100g\nfish / 28THB/100g\nduck / 68THB/100g\n");
           printf("Select Product = ");
-          scanf("%7s", item.product);
-          if (strcmp(item.product, "egg")== 0) {
-              printf("Required Quantity (item.unit) = ");
-              scanf("%d", &item.unit);
-              item.total = item.unit * 6;
-              printf("Total %s = %d THB\n", item.product, item.total);
-          }else if (strcmp(item.product, "pork")== 0) {
+          scanf("%7s", v1.product);
+          if (strcmp(v1.product, "egg")== 0) {
+              printf("Required Quantity (v1.unit) = ");
+              scanf("%d", &v1.unit);
+              v1.total = v1.unit * 6;
+              printf("Total %s = %d THB\n", v1.product, v1.total);
+          }else if (strcmp(v1.product, "pork")== 0) {
               printf("Required Quantity (g) = ");
-              scanf("%d", &item.unit);
-              item.total = (item.unit * 46) / 100;
-              printf("Total %s = %d THB\n", item.product, item.total);
-          }else if (strcmp(item.product, "fish")== 0) {
+              scanf("%d", &v1.unit);
+              v1.total = (v1.unit * 46) / 100;
+              printf("Total %s = %d THB\n", v1.product, v1.total);
+          }else if (strcmp(v1.product, "fish")== 0) {
               printf("Required Quantity (g) = ");
-              scanf("%d", &item.unit);
-              item.total = (item.unit * 28) / 100;
-              printf("Total %s = %d THB\n", item.product, item.total);
-          }else if (strcmp(item.product, "duck")== 0) {
+              scanf("%d", &v1.unit);
+              v1.total = (v1.unit * 28) / 100;
+              printf("Total %s = %d THB\n", v1.product, v1.total);
+          }else if (strcmp(v1.product, "duck")== 0) {
               printf("Required Quantity (g) = ");
-              scanf("%d", &item.unit);
-              item.total = (item.unit * 68) / 100;
-              printf("Total %s = %d THB\n", item.product, item.total);
+              scanf("%d", &v1.unit);
+              v1.total = (v1.unit * 68) / 100;
+              printf("Total %s = %d THB\n", v1.product, v1.total);
           }    
     }else if (select == 2) {
         printf("\n\n[1]Contact [2]Membership\n");
