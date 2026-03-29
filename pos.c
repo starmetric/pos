ec799e16a3b8d516464d2dfdd6a0793edad43d19
starmetric <na0z.lineage2@gmail.com>
2026-03-29 21:35:58 +0700

    Major system upgrade.

--- pos.c
+++ pos.c
@@ -4,12 +4,12 @@
 typedef struct {
     char product[10];
     int unit;
-    int total;
+    float total;
 } structvisit;
 
 typedef struct {
     char productstorage[10];
-    int price;
+    float price;
     char unit[5];
 }storage;
 
@@ -21,33 +21,40 @@ int main(){
 
     storage menu[4] = {
         {"egg", 6, "unit"},
-        {"pork", 46, "g"},
-        {"fish", 28, "g"},
-        {"duck", 68, "g"},
+        {"pork", 0.48, "g"},
+        {"fish", 0.28, "g"},
+        {"duck", 0.68, "g"},
 };
     printf("\tHello, welcome\n\n\tStarmetricshop\n");
 
-    while (running == 1){
+    //while (running == 1){
 
     printf("[1]Productlist [2]Contactmembership [3]Exit\n");
     printf("select Number : ");
     scanf("%d", &select);
-
-    //if (select == 1) {
-    //    if(count == 10) {
-    //       printf("cart is full\n");
-    //      continue;
-    //    }
-    for (int i = 0; i < 4; i++)
-        printf("%s / %dTHB/%s\n", menu[i].productstorage, menu[i].price, menu[i].unit);
-
-    //}else if (select == 2) {
-    //    printf("\n\n[1]Contact [2]Membership\n");
-    //
-    //}else 
-    //    printf("Incorrect information. Please re-enter.\n");
+        
+    if (select == 1) {
+          
+      for (int i = 0; i < 4; i++) {
+        printf("%s / %.2fTHB/%s\n", menu[i].productstorage, menu[i].price, menu[i].unit);  
+        
+      } printf("Please select a menu = ");
+        scanf("%7s", cart[count].product);
+        
+          int found = 0;
+          
+          for (int i = 0; i < 4; i++) {
+            if (strcmp(cart[count].product, menu[i].productstorage)== 0) {
+                printf("Required Quantity (%s) = ", menu[i].unit);
+                scanf("%d", &cart[count].unit);
+                cart[count].total = cart[count].unit * menu[i].price;
+                printf("Total %s = %.2f THB\n", menu[i].productstorage, cart[count].total);
+                break;
+            }
+          }
+        if (!found) {
+            printf("Product not found.\n");
+        }  
     }
-
-    
     return 0;
 }
