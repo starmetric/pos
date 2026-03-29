9c7a631ef7614f6f53d9ae9e68342e27ce74b4e8
starmetric <na0z.lineage2@gmail.com>
2026-03-29 22:17:02 +0700

    upgrade. Line2

--- pos.c
+++ pos.c
@@ -16,18 +16,18 @@ typedef struct {
 int main(){
     structvisit cart[10];
     int count = 0;
+    
     int select;
-    int running = 1;
 
     storage menu[4] = {
         {"beef", 98, "g"},
         {"pork", 48, "g"},
         {"fish", 28, "g"},
         {"duck", 68, "g"},
-};
+    };
+    
     printf("\tHello, welcome\n\n\tStarmetricshop\n");
 
-    //while (running == 1){
 
     printf("[1]Productlist [2]Contactmembership [3]Exit\n");
     printf("select Number : ");
@@ -35,27 +35,27 @@ int main(){
         
     if (select == 1) {
 
-     int found = 0; 
-     while (!found) {  
-      printf("\n\n--------------Product-------------\n\n");
-      for (int i = 0; i < 4; i++) {
-        printf("%s / %.2fTHB/%s\n", menu[i].productstorage, menu[i].price, menu[i].unit);  
+        int found = 0; 
+        while (!found) {  
+        printf("\n\n--------------Product-------------\n\n");
+        for (int i = 0; i < 4; i++) {
+            printf("%s / %.2fTHB/%s\n", menu[i].productstorage, menu[i].price, menu[i].unit);  
         
-      } printf("Please select a menu = ");
+        } printf("Please select a menu = ");
         scanf("%7s", cart[count].product);
-        
-          for (int i = 0; i < 4; i++) {
-            if (strcmp(cart[count].product, menu[i].productstorage)== 0) {
-                printf("Required Quantity (%s) = ", menu[i].unit);
-                scanf("%f", &cart[count].unit);
-                cart[count].total = (cart[count].unit / 100) * menu[i].price;
-                printf("Total %s = %.2f THB\n", menu[i].productstorage, cart[count].total);
-                break;
+            
+            for (int i = 0; i < 4; i++) {
+                if (strcmp(cart[count].product, menu[i].productstorage)== 0) {
+                    printf("Required Quantity (%s) = ", menu[i].unit);
+                    scanf("%f", &cart[count].unit);
+                    cart[count].total = (cart[count].unit / 100) * menu[i].price;
+                    printf("Total %s = %.2f THB\n", menu[i].productstorage, cart[count].total);
+                    break;
+                }
             }
-          }
         if (!found) {
         }  
-     }   
+        }
     }
     return 0;
 }
