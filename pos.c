d583ab7a33a3f0d947bbb1ed5c2cdaa8c6671722
starmetric <na0z.lineage2@gmail.com>
2026-03-29 22:15:40 +0700

    upgrade. Line

--- pos.c
+++ pos.c
@@ -3,7 +3,7 @@
 
 typedef struct {
     char product[10];
-    int unit;
+    float unit;
     float total;
 } structvisit;
 
@@ -20,10 +20,10 @@ int main(){
     int running = 1;
 
     storage menu[4] = {
-        {"egg", 6, "unit"},
-        {"pork", 0.48, "g"},
-        {"fish", 0.28, "g"},
-        {"duck", 0.68, "g"},
+        {"beef", 98, "g"},
+        {"pork", 48, "g"},
+        {"fish", 28, "g"},
+        {"duck", 68, "g"},
 };
     printf("\tHello, welcome\n\n\tStarmetricshop\n");
 
@@ -34,27 +34,28 @@ int main(){
     scanf("%d", &select);
         
     if (select == 1) {
-          
+
+     int found = 0; 
+     while (!found) {  
+      printf("\n\n--------------Product-------------\n\n");
       for (int i = 0; i < 4; i++) {
         printf("%s / %.2fTHB/%s\n", menu[i].productstorage, menu[i].price, menu[i].unit);  
         
       } printf("Please select a menu = ");
         scanf("%7s", cart[count].product);
         
-          int found = 0;
-          
           for (int i = 0; i < 4; i++) {
             if (strcmp(cart[count].product, menu[i].productstorage)== 0) {
                 printf("Required Quantity (%s) = ", menu[i].unit);
-                scanf("%d", &cart[count].unit);
-                cart[count].total = cart[count].unit * menu[i].price;
+                scanf("%f", &cart[count].unit);
+                cart[count].total = (cart[count].unit / 100) * menu[i].price;
                 printf("Total %s = %.2f THB\n", menu[i].productstorage, cart[count].total);
                 break;
             }
           }
         if (!found) {
-            printf("Product not found.\n");
         }  
+     }   
     }
     return 0;
 }
